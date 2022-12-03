2758424c081897d1a592b9a1c0bea23e3cc084c0
BENwada <domo0103wada@gmail.com>
2022-12-03 14:15:35 +0900

    homePage added

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -4,6 +4,7 @@ import "./Slider.scss";
 
 const Slider = () => {
   const [currentSlide, setCurrentSlide] = useState(0);
+
   const data = [
     "https://images.pexels.com/photos/1549200/pexels-photo-1549200.jpeg?auto=compress&cs=tinysrgb&w=1600",
     "https://images.pexels.com/photos/949670/pexels-photo-949670.jpeg?auto=compress&cs=tinysrgb&w=1600",
